e7a6f811a8bb725e8652db7a7d04c9a6d404aa53
Samuel Sampeur <sampeur4life1@gmail.com>
2022-03-15 00:31:44 -0700

    Diferent kind of particles able particlesInit and particlesLoaded

--- src/config/Home.jsx
+++ src/config/Home.jsx
@@ -2,259 +2,107 @@ import React from "react";
 import Particles from 'react-tsparticles';
 
 function Header() {
+
+        const particlesInit = (main) => {
+          console.log(main);
+      
+          // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        };
+      
+        const particlesLoaded = (container) => {
+          console.log(container);
+        };
+
+
+
   return (
     <div className="back">
         <Particles
-          class="particulas"
+
+
           id="tsparticles"
-          options={{
-            background: {
-              color: {
-                value: ""
-              },
-              image: 'url("https://mapio.net/images-p/2150197.jpg")',
-              position: "center",
-              repeat: "",
-              size: "",
-              opacity: 1
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#0d47a1",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
             },
-            backgroundMask: {
-              cover: {
-                color: {
-                  value: "#fff"
-                },
-                opacity: 1
-              },
-              enable: false
+            onHover: {
+              enable: true,
+              mode: "repulse",
             },
-            detectRetina: true,
-            fpsLimit: 330,
-            infection: {
-              cure: false,
-              delay: 0,
-              enable: false,
-              infections: 0,
-              stages: []
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
             },
-            interactivity: {
-              detectsOn: "canvas",
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: []
-                },
-                onDiv: {
-                  ids: [],
-                  enable: false,
-                  mode: [],
-                  type: "circle"
-                },
-                onHover: {
-                  enable: false,
-                  mode: [],
-                  parallax: {
-                    enable: true,
-                    force: 500,
-                    smooth: 10
-                  }
-                },
-                resize: true
-              },
-              modes: {
-                attract: {
-                  distance: 20,
-                  duration: 0.1,
-                  speed: 1
-                },
-                bubble: {
-                  distance: 200,
-                  duration: 0.4
-                },
-                connect: {
-                  distance: 40,
-                  links: {
-                    opacity: 0.5
-                  },
-                  radius: 60
-                },
-                grab: {
-                  distance: 100,
-                  links: {
-                    opacity: 1
-                  }
-                },
-                push: {
-                  quantity: 4
-                },
-                remove: {
-                  quantity: 2
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                  speed: 1
-                },
-                slow: {
-                  factor: 3,
-                  radius: 200
-                },
-                trail: {
-                  delay: 1,
-                  quantity: 1
-                }
-              }
+            push: {
+              quantity: 4,
             },
-            particles: {
-              collisions: {
-                enable: true,
-                mode: "bounce"
-              },
-              color: {
-                value: "#fff",
-                animation: {
-                  enable: false,
-                  speed: 1,
-                  sync: true
-                }
-              },
-              links: {
-                blink: false,
-                color: {
-                  value: "#fff"
-                },
-                consent: false,
-                distance: 200,
-                enable: true,
-                opacity: 1,
-                shadow: {
-                  blur: 5,
-                  color: {
-                    value: "#00ff00"
-                  },
-                  enable: false
-                },
-                triangles: {
-                  enable: false
-                },
-                width: 1,
-                warp: false
-              },
-              move: {
-                angle: 90,
-                attract: {
-                  enable: false,
-                  rotate: {
-                    x: 3000,
-                    y: 3000
-                  }
-                },
-                direction: "none",
-                enable: true,
-                noise: {
-                  delay: {
-                    random: {
-                      enable: false,
-                      minimumValue: 0
-                    },
-                    value: 0
-                  },
-                  enable: false
-                },
-                outMode: "out",
-                random: false,
-                speed: 2,
-                straight: false,
-                trail: {
-                  enable: false,
-                  length: 10,
-                  fillColor: {
-                    value: "#000000"
-                  }
-                },
-                vibrate: false,
-                warp: false
-              },
-              number: {
-                density: {
-                  enable: false,
-                  area: 800,
-                  factor: 1000
-                },
-                limit: 200,
-                value: 50
-              },
-              opacity: {
-                animation: {
-                  enable: false,
-                  minimumValue: 0,
-                  speed: 2,
-                  sync: false
-                },
-                random: {
-                  enable: false,
-                  minimumValue: 1
-                },
-                value: 1
-              },
-              rotate: {
-                animation: {
-                  enable: false,
-                  speed: 0,
-                  sync: false
-                },
-                direction: "clockwise",
-                path: false,
-                random: false,
-                value: 0
-              },
-              shadow: {
-                blur: 0,
-                color: {
-                  value: "#000000"
-                },
-                enable: false,
-                offset: {
-                  x: 0,
-                  y: 0
-                }
-              },
-              shape: {
-                options: {},
-                type: "circle"
-              },
-              size: {
-                animation: {
-                  destroy: "none",
-                  enable: false,
-                  minimumValue: 0,
-                  speed: 5,
-                  startValue: "max",
-                  sync: false
-                },
-                random: {
-                  enable: false,
-                  minimumValue: 1
-                },
-                value: 2
-              },
-              stroke: {
-                width: 0
-              },
-              twinkle: {
-                lines: {
-                  enable: false,
-                  frequency: 0.05,
-                  opacity: 1
-                },
-                particles: {
-                  enable: false,
-                  frequency: 0.05,
-                  opacity: 1
-                }
-              }
+            repulse: {
+              distance: 200,
+              duration: 0.4,
             },
-            pauseOnBlur: true
-          }}
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+
+
+
         />
         
     </div>
