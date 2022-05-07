cee75f4f298c96750886e6a64ef30b0330e89e7b
Samuel Sampeur <sampeur4life1@gmail.com>
2022-05-06 23:43:04 -0700

    Const file for congugurations and best pratice

---
+++ src/consts.js
@@ -0,0 +1,28 @@
+export const Instagram = "https://www.instagram.com";
+export const Facebook = "https://web.facebook.com";
+export const Twitter = "https://twitter.com";
+export const GitHub = "https://github.com";
+export const LinkedIn = "https://www.linkedin.com";
+export const Google = "https://mail.google.com/mail/u/0/#inbox?compose=new";
+export const Behance = "https://www.behance.net/gallery/58902999/Login";
+export const Dribbble = "https://dribbble.com/session/new";
+
+export const UrlSocialMedia = {
+    User1Insta: Instagram + "/samuel.louis1991/",
+    User1Facebook: Facebook + "",
+    User1Twitter: Twitter + "/in/louis-samuel-544082110/",
+    User1GitHub: GitHub + "/in/louis-samuel-544082110/",
+    User1LinkedIn: LinkedIn + "/in/louis-samuel-544082110/",
+    User1Google: Google + "/in/louis-samuel-544082110/",
+    User1Behance: Behance + "/in/louis-samuel-544082110/",
+    User1Dribbble: Dribbble + "/in/louis-samuel-544082110/",
+
+    User2Insta: Instagram + "/samuel.louis1991/",
+    User2Facebook: Facebook + "",
+    User2Twitter: Twitter + "/in/louis-samuel-544082110/",
+    User2GitHub: GitHub + "/in/louis-samuel-544082110/",
+    User2LinkedIn: LinkedIn + "/in/louis-samuel-544082110/",
+    User2Google: Google + "/in/louis-samuel-544082110/",
+    User2Behance: Behance + "/in/louis-samuel-544082110/",
+    User2Dribbble: Dribbble + "/in/louis-samuel-544082110/",
+}
