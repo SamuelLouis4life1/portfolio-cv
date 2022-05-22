e47a867191d458b6f21a2e7ae724c7385889b544
Samuel Sampeur <sampeur4life1@gmail.com>
2022-05-21 21:17:06 -0700

    Service section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import '../css/services.css'
+import React, { Component, useState } from 'react';
+import '../css/services.css';
+import Testimonial from "./Testimonial"
+import CardServices from "./CardServices"
 import { withTranslation } from "react-i18next";
 
-export function Services (props) {
-    const { t } = props;    
+export function Services(props) {
+    const { t } = props;
+
+    // const [btnExample, setbtnExample] = useState({`${t("services.subTitle")}`});
+    // btnExample = {{t("services.title")}
 
     return (
 
-        <section className="services-setion">
+        <section className="services-setion" id='services'>
+            <h1 className="title">{t("services.title")}</h1>
+                        
+            <p className="subtitle">{t("services.subTitle")}</p>
 
-            <div className="services-description">
-
-                <h5>{t("services.description")} </h5>
+            <div className="service_container">
+                <CardServices title={t("services.designer")} btnLink={t("services.subTitle")} paragraph={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa."} />
+                <CardServices title={t("services.uiUxApp")} btnLink={t("services.subTitle")} paragraph={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa."} />
+                <CardServices title={t("services.uiUxWeb")} btnLink={t("services.subTitle")} paragraph={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa."} />
+                <CardServices title={"services.webDesign"} btnLink={t("services.subTitle")} paragraph={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa."} />
             </div>
+
+            <Testimonial/>
 
         </section>
     );
